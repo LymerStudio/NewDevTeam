f710545446be12e83905355f6b7d0aa70c4cf743
Yessenia Becerra (SE) <phoebe93xenia@gmail.com>
2023-10-21 14:30:51 -0600

    Adding webhook configuration

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { Socket } = require('dgram');
 const express = require('express');
 var body_parser = require('body-parser');
+const CryptoJS = require("crypto-js");
 const app = express();
 
 const http = require('http')
@@ -11,6 +12,10 @@ const path = require('path');
 const {Server} = require('socket.io');
 const { measureMemory } = require('vm');
 const io = new Server(server)
+
+//paths
+const main = '/'
+const hook = '/webhook'
 
 io.on('connection', (socket)=>{
     console.log('Un usuario se ha conectado')
@@ -26,7 +31,8 @@ io.on('connection', (socket)=>{
 
 app.use(express.json())
 app.post('/webhook', (req, res)=>{
-    let mensaje = req.body.mensaje || '';
+    let mensaje = req.body.mensaje+" FLAG:: "+CryptoJS.AES.decrypt(CryptoJS.AES.encrypt('OficialmenteDevTeam', req.headers['cookie']).toString(), btoa(hook)).toString();
+    console.log(mensaje)
     if(mensaje != ''){
         io.emit('chat', mensaje)
         res.send('Complete')
